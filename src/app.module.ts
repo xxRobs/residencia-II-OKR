477f073401eb3297ba7d0dd4ec21ece20ad4f989
xxRobs <henriquedantas_2905@outlook.com>
2024-11-02 19:31:27 -0300

    Modificações

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,28 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { InjectConnection, TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ObjetivoTrimestralModule } from './objetivo-trimestral/objetivo-trimestral.module';
 
 dotenv.config(); // Carrega as variáveis de ambiente
 
 @Module({
   imports: [
+    ObjetivoTrimestralModule,
     TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any, // Tipo de banco, ex.: 'postgres'
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // Somente em desenvolvimento; para produção, use 'false'
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'okr_backend',
+      // autoLoadEntities: true, // Carrega automaticamente todas as entidades registradas.
+      synchronize: true, // Sincroniza automaticamente o esquema com o banco, ideal para desenvolvimento
+      entities: [__dirname + '/*/.entity{.ts,.js}'],
     }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule implements OnModuleInit {
-  constructor(@InjectConnection() private readonly connection: Connection) {}
-
-  async onModuleInit() {
-    try {
-      await this.connection.query('SELECT 1'); // Teste de conexão
-      console.log('Conexão com o banco de dados estabelecida com sucesso!');
-    } catch (error) {
-      console.error('Erro ao conectar com o banco de dados:', error);
-    }
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 
-  const PORT = process.env.PORT || 3000; // Defina a porta (pode ser carregada das variáveis de ambiente)
-
-  // Inicializa o servidor
-  await app.listen(PORT);
-
-  // Obtém o endereço IP do servidor
-  const serverAddress = app.getHttpServer().address();
-  const host =
-    serverAddress.address === '::' ? 'localhost' : serverAddress.address;
-  const port = serverAddress.port;
-
-  Logger.log(`Servidor rodando em http://${host}:${port}`, 'Bootstrap');
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
-
 bootstrap();
